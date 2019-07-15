3f86a0e308072d15ffd2fe3089b6a5734d3610ee
litao918 <48230772+litao918@users.noreply.github.com>
2019-07-15 19:24:27 +0800

    litao

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 App({
   onLaunch: function () {
     // 展示本地存储能力
+    wx.setStorageSync('language', '简体中文')
     var logs = wx.getStorageSync('logs') || []
     logs.unshift(Date.now())
     wx.setStorageSync('logs', logs)
