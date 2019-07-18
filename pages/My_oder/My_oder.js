930f9282daaa4a8ff48d1491d86e8902b2432eb7
qingjueaaa@163.com <qingjueaaa@163.com>
2019-07-18 18:48:34 +0800

    zy

--- pages/My_oder/My_oder.js
+++ pages/My_oder/My_oder.js
@@ -1,11 +1,30 @@
 // pages/My_oder/My_oder.js
+const app = getApp()
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    tabrid:0
+    tabrid:0,
+    selected: '',//选中语言状态
+    chinese: {
+      category1:"全部",
+      category2:"待提货",
+      category3:"已完成",
+      ordertime:"下单时间:",
+      anotherlist:"再来一单"
+    },//中文包
+
+    english: {
+      category1: "All",
+      category2: "For pickup",
+      category3: "Done",
+      ordertime: "order time:  ",
+      anotherlist: "Another list"
+    },//英文包
+
+    selectpackage: ''//当前选中的语言包
   },
 
   xuanxiangka: function (e) {
@@ -34,9 +53,26 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-
+    this.setData({
+      selected: app.globalData.language
+    })
+    this.selectLanguagePack()
   },
 
+  //语言包的选择
+  selectLanguagePack() {
+    if (this.data.selected == 0) {
+      var data = this.data.english
+      this.setData({
+        selectpackage: data
+      })
+    } else if (this.data.selected == 1) {
+      var data = this.data.chinese
+      this.setData({
+        selectpackage: data
+      })
+    }
+  },
   /**
    * 生命周期函数--监听页面隐藏
    */
