d77314d740899cee9abc2073153f5f2b07a0606e
qingjueaaa@163.com <qingjueaaa@163.com>
2019-07-19 18:40:02 +0800

    zy

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,207 +1,17 @@
 //index.js
 //获取应用实例
 const app = getApp()
+var call = require("../../utils/api.js")
 Page({
   data: {
+    banner:{},//轮播图数据
+    selsected:"",//选中当前语言状态
     classfiySelect: "",
-    leftText: [{
-      id: "01",
-      text1: "美妆专区",
-    },
-    {
-      id: "02",
-      text1: "面部清洁",
-    },
-    {
-      id: "03",
-      text1: "洗护专区",
-    },
-    {
-      id: "04",
-      text1: "面膜",
-    },
-    {
-      id: "05",
-      text1: "口红",
-    },
-    {
-      id: "06",
-      text1: "美妆专区",
-    },
-   
- 
-    ],
-    rightData: [{
-      id: "01",
-      title: "美妆专区",
-      frist: [{
-        url: "/images/85309.jpg",
-        text: "卡姿兰补水套装",
-        money: 200,
-        sum: 20,
-        id: 1,
-      },
-      {
-        url: "/images/85309.jpg",
-        text: "卡姿兰补水套装",
-        money: 200,
-        sum: 20,
-        id: 2,
-      },
-      {
-        url: "/images/85309.jpg",
-        text: "卡姿兰补水套装",
-        money: 200,
-        sum: 20,
-        id: 2,
-      },
-      {
-        url: "/images/85309.jpg",
-        text: "卡姿兰补水套装",
-        money: 200,
-        sum: 20,
-        id: 2,
-      },
-      {
-        url: "/images/85309.jpg",
-        text: "卡姿兰补水套装",
-        money: 200,
-        sum: 20,
-        id: 2,
-      },
-      {
-        url: "/images/85309.jpg",
-        text: "卡姿兰补水套装",
-        money: 200,
-        sum: 20,
-      },
-      ],
-    },
-    {
-      id: "02",
-      title: "面部清洁",
-      frist: [{
-        url: "/images/85309.jpg",
-        text: "卡姿兰补水套装",
-        money: 200,
-        sum: 20,
-      },
-      {
-        url: "/images/85309.jpg",
-        text: "卡姿兰补水套装",
-        money: 200,
-        sum: 20,
-      },
-      {
-        url: "/images/85309.jpg",
-        text: "卡姿兰补水套装",
-        money: 200,
-        sum: 20,
-      },
-      ],
-    },
-    {
-      id: "03",
-      title: "洗护专区",
-      frist: [{
-        url: "/images/85309.jpg",
-        text: "卡姿兰补水套装",
-        money: 200,
-        sum: 20,
-      },
-      {
-        url: "/images/85309.jpg",
-        text: "卡姿兰补水套装",
-        money: 200,
-        sum: 20,
-      },
-      {
-        url: "/images/85309.jpg",
-        text: "卡姿兰补水套装",
-        money: 200,
-        sum: 20,
-      },
-      ],
-    },
-    {
-      id: "04",
-      title: "面膜",
-      frist: [{
-        url: "/images/85309.jpg",
-        text: "卡姿兰补水套装",
-        money: 200,
-        sum: 20,
-      },
-      {
-        url: "/images/85309.jpg",
-        text: "卡姿兰补水套装",
-        money: 200,
-        sum: 20,
-      },
-      {
-        url: "/images/85309.jpg",
-        text: "卡姿兰补水套装",
-        money: 200,
-        sum: 20,
-      },
-      ],
-    },
-    {
-      id: "05",
-      title: "口红",
-      frist: [{
-        url: "/images/85309.jpg",
-        text: "卡姿兰补水套装",
-        money: 200,
-        sum: 20,
-      },
-      {
-        url: "/images/85309.jpg",
-        text: "卡姿兰补水套装",
-        money: 200,
-        sum: 20,
-      },
-      {
-        url: "/images/85309.jpg",
-        text: "卡姿兰补水套装",
-        money: 200,
-        sum: 20,
-      },
-      ],
-    },
-    {
-      id: "06",
-      title: "美妆专区",
-      frist: [{
-        url: "/images/85309.jpg",
-        text: "卡姿兰补水套装",
-        money: 200,
-        sum: 20,
-      },
-      {
-        url: "/images/85309.jpg",
-        text: "卡姿兰补水套装",
-        money: 200,
-        sum: 20,
-      },
-      {
-        url: "/images/85309.jpg",
-        text: "卡姿兰补水套装",
-        money: 20088,
-        sum: 20,
-      },
-      {
-        url: "/images/85309.jpg",
-        text: "卡姿兰补水套装",
-        money: 20088,
-        sum: 20,
-      },
-  
-      ],
-    },
-    ],
+    leftText: [],//商品分类字段
+    rightData: [],//商品列表字段
     scrollTop:'',
-    navid:'0'
+    navid:'0',
+
   },
 
   navClick:function(e){
@@ -214,15 +24,135 @@ Page({
 
   onLoad: function () {
     // console.log("我是"+language)
-    this.setData({
-      classfiySelect: this.data.leftText[0].id
-    })
   },
   onShow:function(){
-    const language = app.globalData.language
-    console.log("我是"+language)
+   this.setData({
+     selsected: app.globalData.language
+   })
+    this.getbanner();//获取banner图
+    this.gettype();//获取商品分类
+    this.getgoods();//获取商品列表
+    // console.log(this.data.leftTex)
+
   },
 
+  // 得到banner图
+  getbanner(){
+    var url ="appi/index/banner"
+    var data ='';
+    call.request(url,data,this.getbannerSuccess,this.getbannerFail)
+  },
+
+  // banner图请求成功回调函数
+  getbannerSuccess(res){
+    console.log(res)
+    if(res.code==1){
+      this.setData({
+        banner:res.data
+      })
+    }else{
+      wx.showToast({
+        title: res.msg,
+        icon: 'none',
+        duration: 1500
+      })
+    }
+  },
+  // banner图请求失败回调函数
+  getbannerFail() {
+    wx.showToast({
+      title: "轮播图请求失败",
+      icon: 'none',
+      duration: 1500
+    })
+  },
+
+  // 获取商品分类
+  gettype(){
+    var url = "appi/index/sp_fenlei"
+    var data ={
+      type:this.data.selsected
+    }
+    call.request(url, data, this.gettypeSuccess, this.gettypeFail)
+  },
+  //商品分类请求成功回调函数
+  gettypeSuccess(res){
+    console.log(res)
+    console.log()
+    if (res.code == 1) {
+      this.setData({
+        leftText: res.data
+      })
+      this.setData({
+        classfiySelect: this.data.leftText[0].id
+      })
+    } else {
+      wx.showToast({
+        title: res.msg,
+        icon: 'none',
+        duration: 1500
+      })
+    }
+  },
+
+  //商品分类请求失败回调函数
+  gettypeFail(){
+    wx.showToast({
+      title: "商品分类请求失败",
+      icon: 'none',
+      duration: 1500
+    })
+  },
+
+  //获取商品列表
+  getgoods() {
+    var url = "appi/index/sp_list_one"
+    var data = {
+      type: this.data.selsected
+    }
+    call.request(url, data, this.getgoodsSuccess, this.getgoodsFail)
+  },
+
+  //商品列表请求成功回调函数
+  getgoodsSuccess(res){
+    console.log(res)
+    if (res.code == 1) {
+      res.data.map((item,index)=>{
+        // console.log(item);
+        item.shop.map((val,i)=>{
+          console.log(val);
+          val['num']=0;
+        })
+      })//在商品列表中添加购买数量属性
+      console.log(res.data);
+      this.setData({
+        rightData:res.data
+      })
+    } else {
+      wx.showToast({
+        title: res.msg,
+        icon: 'none',
+        duration: 1500
+      })
+    }
+  },
+  //商品列表请求失败回调函数
+  getgoodsFail(){
+
+  },
+  // 增加购买数量
+  add(e){
+    console.log(e);
+    var key1 = e.currentTarget.dataset.index2//外层商品列表
+    var key2 = e.currentTarget.dataset.index1//里层商品详情
+    var data = this.data.rightData;
+    data[key1].shop[key2].num += 1;
+    this.setData({
+       rightData:data
+    })
+    ;
+    console.log(this.data.rightData);
+  },
   onPageScroll: function (e) {
     //console.log(e.scrollTop)
     var that = this
@@ -231,15 +161,14 @@ Page({
     })
   },
 
-
   //滚动触发
   scroll: function (e) {
-  
-    var scrollTop = e.detail.scrollTop,
-      h = 0,
-      classfiySelect;
+    var scrollTop = e.detail.scrollTops
+    var  h = 0;
+    var classfiySelect="";
     var that = this;
     that.data.leftText.forEach(function (clssfiy, i) {
+      console.log(clssfiy);
       var _h = 26 + that.length(clssfiy['id']) * 102;
       if (scrollTop >= h) {
         classfiySelect = clssfiy['id'];
